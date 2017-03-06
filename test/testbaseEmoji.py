24cd30d4d8476a2fcd5d6c74a3adcf0d7682656f
TsunamiNoAi <tsunaminoai@gmail.com>
2017-03-05 23:34:32 -0500

    Add failure messages for debugging

--- test/testbaseEmoji.py
+++ test/testbaseEmoji.py
@@ -11,24 +11,24 @@ class baseEmojiTest(unittest.TestCase):
         test_int = random.getrandbits(128)
         encoded_string = baseEmoji.encode(test_int)
         decoded_int = baseEmoji.decode(encoded_string)
-        self.assertEqual(test_int, decoded_int)
+        self.assertEqual(test_int, decoded_int, "Failed on %d" % test_int)
 
     def testEncodeDecodeString(self):
         for i in range(100):
             test_string = ''.join(random.choice(string.printable) for _ in range(random.randint(10,140)))
             encoded_string = baseEmoji.encodeStr(test_string)
             decoded_string = baseEmoji.decodeStr(encoded_string)
-            self.assertEqual(test_string, decoded_string)
+            self.assertEqual(test_string, decoded_string, "Failed on %s" % test_string)
 
 
     def testEncodeDecodeStringWithSpacing(self):
         test_string = ''.join(random.choice(string.printable) for _ in range(random.randint(10,140)))
         encoded_string = baseEmoji.encodeStr(test_string," ")
         decoded_string = baseEmoji.decodeStr(encoded_string," ")
-        self.assertEqual(test_string, decoded_string)
+        self.assertEqual(test_string, decoded_string, "Failed on %s" % test_string)
 
     def testEncodeDecodeIntWithSpacing(self):
         test_int = random.getrandbits(128)
         encoded_string = baseEmoji.encode(test_int," ")
         decoded_int = baseEmoji.decode(encoded_string," ")
-        self.assertEqual(test_int, decoded_int)
+        self.assertEqual(test_int, decoded_int, "Failed on %d" % test_int)
